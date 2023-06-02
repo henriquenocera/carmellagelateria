6a6998f33bfb9b06a6a4ab82bd4b34370d85de5b
Henrique Nocera <henrique@axioscorp.com.br>
2023-06-02 16:52:42 -0300

    index

--- src/routes/ChecklistFechamento.tsx
+++ src/routes/ChecklistFechamento.tsx
@@ -1,23 +1,143 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as Icons from "react-icons/bs";
 
 const telegramBotId = "5635956016:AAFzevSjVPEhTVsOEfLpbUsT0jni93pG6-c";
 const telegramChatId = "-1001602173856";
 
+async function sendOpenMessage(openDateFormat) {
+  const checkOpenComplete = `https://api.telegram.org/bot${telegramBotId}/sendMessage?chat_id=${telegramChatId}&text=Checklist de Abertura - Loja Alto XV %0D%0A ${openDateFormat}`;
+  try {
+    const response = await fetch(checkOpenComplete, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({}),
+    });
+    const data = await response.json();
+    console.log(JSON.stringify(data));
+    window.location.reload();
+  } catch (error) {
+    console.error(error);
+    window.alert(
+      "Erro na confirmação do Checklist, por gentileza tente novamente"
+    );
+  }
+}
+function getLocalStorage() {
+  let today = new Date();
+  let open = JSON.parse(localStorage.getItem("altoxvOpen"));
+  if (open) {
+    open.timestamp = open.timestamp;
+    console.log(open.timestamp);
+  } else {
+    open = "Ainda nao completo";
+  }
+  let openDateFormat = new Date(open.timestamp);
+  openDateFormat =
+    "Checklist Completo em: " +
+    openDateFormat.getDate() +
+    "/" +
+    openDateFormat.getMonth() +
+    "/" +
+    openDateFormat.getFullYear() +
+    " -- " +
+    openDateFormat.getHours() +
+    ":" +
+    openDateFormat.getMinutes() +
+    ":" +
+    openDateFormat.getSeconds();
+  console.log(openDateFormat);
+
+  let dayToday = today.getDate();
+  let lastDayComplete = new Date(open.timestamp).getDate();
+
+  /*   if (dayToday == lastDayComplete) {
+    console.log("Stop Form"); */
+  // Checklist already complete today
+  // Block Form
+  /*     form.addEventListener("submit", (e) => {
+      e.preventDefault();
+    }); */
+
+  /*     let inputs = form.querySelectorAll(".inp-cbx");
+    inputs.forEach((input) => {
+      input.checked = true;
+      input.disabled = true;
+    }); */
+
+  /*     let button = form.querySelector(".submit");
+    button.disabled = true;
+  } else {
+    let e = document.querySelector("#timestamp");
+    e.innerHTML = "";
+  } */
+}
+getLocalStorage();
+function altoxvOpenSubmit() {
+  const currentDate = new Date();
+  console.log(currentDate);
+
+  var object = { value: "complete", timestamp: new Date().getTime() };
+  localStorage.setItem("altoxvOpen", JSON.stringify(object));
+
+  let openDateFormat = new Date(object.timestamp);
+  openDateFormat =
+    openDateFormat.getDate() +
+    "/" +
+    openDateFormat.getMonth() +
+    "/" +
+    openDateFormat.getFullYear() +
+    " -- " +
+    openDateFormat.getHours() +
+    ":" +
+    openDateFormat.getMinutes() +
+    ":" +
+    openDateFormat.getSeconds();
+  sendOpenMessage(openDateFormat);
+}
+
 const onSubmit = (e) => {
   console.log("enviou");
   e.preventDefault();
-
-  fetch(
-    `https://api.telegram.org/bot${telegramBotId}/sendMessage?chat_id=${telegramChatId}&text=Checklist de Fechamento - Loja Alto XV %0D%0A`
-  );
+  altoxvOpenSubmit();
 };
 
 function ChecklistFechamento() {
+  const [timeComplete, setTimeComplete] = useState(false);
+
+  useEffect(() => {
+    let open = JSON.parse(localStorage.getItem("altoxvOpen"));
+    if (open) {
+      open.timestamp = open.timestamp;
+      console.log(open.timestamp);
+    } else {
+      open = "Ainda nao completo";
+    }
+    let openDateFormat = new Date(open.timestamp);
+    openDateFormat =
+      "Checklist Completo em: " +
+      openDateFormat.getDate() +
+      "/" +
+      openDateFormat.getMonth() +
+      "/" +
+      openDateFormat.getFullYear() +
+      " -- " +
+      openDateFormat.getHours() +
+      ":" +
+      openDateFormat.getMinutes() +
+      ":" +
+      openDateFormat.getSeconds();
+    console.log(openDateFormat);
+    setTimeComplete(openDateFormat);
+  }, []);
+
   return (
     <div className="checklistContainer">
       <div className="unitContainer">
         <div className="unitInfo">
+          <span id="timestamp">{timeComplete}</span>
           <h1>Checklist de Fechamento</h1>
           <h2>Unidade Alto da XV - Rua Sete de Abril, 934</h2>
         </div>
@@ -25,6 +145,7 @@ function ChecklistFechamento() {
           <img src="/logo.svg" alt="" />
         </div>
       </div>
+
       <div className="warningContainer">
         <p className="warningText">
           <span className="warningIcon">
