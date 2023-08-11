49378ec2e58881b6de61b4c4c4a2a41d27921705
Henrique Nocera <henrique@axioscorp.com.br>
2023-08-10 22:32:08 -0300

    vale

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import ChecklistFechamento from "./routes/ChecklistFechamento.tsx";
 import NavBar from "./components/Navbar";
 import ValeGelato from "./routes/ValeGelato.tsx";
 import Perdas from "./routes/Perdas.tsx";
+import Vales from "./routes/Vales.tsx";
 
 const AppLayout = () => (
   <>
@@ -25,6 +26,7 @@ const router = createBrowserRouter(
     <Route element={<AppLayout />}>
       <Route path="/" element={<Home />} />
       <Route path="/vale-gelato" element={<ValeGelato />} />
+      <Route path="/vales" element={<Vales />} />
       <Route path="/perdas" element={<Perdas />} />
       <Route path="/checklist-abertura" element={<ChecklistAbertura />} />
       <Route path="/checklist-fechamento" element={<ChecklistFechamento />} />
---
+++ src/routes/Vales.tsx
@@ -0,0 +1,158 @@
+import React, { useEffect, useRef, useState } from "react";
+import "../css/Valegelato.css";
+
+const telegramBotId = "5635956016:AAFzevSjVPEhTVsOEfLpbUsT0jni93pG6-c";
+const telegramChatId = "-1001602173856";
+
+const unidadeText = "Ahu";
+const unidade = "ahu";
+
+function Vales() {
+  const [user, setUser] = useState("");
+  const idInputRef = useRef(null);
+  const [item, setItem] = useState("");
+  const [isFormSending, setIsFormSending] = useState(false);
+
+  async function sendValeMessage(openDateFormat) {
+    const checkOpenComplete = `https://api.telegram.org/bot${telegramBotId}/sendMessage?chat_id=${telegramChatId}&text=Vale Gelato - Loja ${unidadeText} %0D%0A ${openDateFormat} %0D%0A ${user}`;
+    try {
+      const response = await fetch(checkOpenComplete, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({}),
+      });
+      const data = await response.json();
+      console.log(JSON.stringify(data));
+    } catch (error) {
+      console.error(error);
+      window.alert("Erro no Envio, por gentileza tente novamente");
+    }
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    let idInput = idInputRef.current.value;
+
+    if (idInput == 270312) {
+      setUser("Henrique");
+    } else if (idInput == 1727) {
+      setUser("Marina");
+    } else if (idInput == 6532) {
+      setUser("Nicolas");
+    } else if (idInput == 8734) {
+      setUser("Monica");
+    } else {
+      setUser("");
+    }
+  }
+
+  function handleItem(e) {
+    console.log(e.target);
+    setItem(true);
+  }
+
+  function sendGoogleSheetData(e) {
+    console.log("enviou");
+    setIsFormSending(true);
+    let openDateFormat = new Date();
+    openDateFormat =
+      openDateFormat.getDate() +
+      "/" +
+      (openDateFormat.getMonth() + 1) +
+      "/" +
+      openDateFormat.getFullYear() +
+      " -- " +
+      openDateFormat.getHours() +
+      ":" +
+      openDateFormat.getMinutes() +
+      ":" +
+      openDateFormat.getSeconds();
+    // sendValeMessage(openDateFormat);
+    e.preventDefault();
+
+    const action = e.target.action;
+    const form = document.getElementById("valeGelatoForm");
+    const data = new FormData(form);
+    fetch(action, {
+      method: "POST",
+      body: data,
+    }).then(() => {
+      setIsFormSending("Sucesso!");
+      window.location.replace(`https://${unidade}.carmellagelateria.com.br/`);
+    });
+  }
+  return (
+    <>
+      <div className="valegelatoContainer">
+        <div className="top">
+          <img className="logo" src="/logo.svg" alt="" />
+          <h1>Vales</h1>
+          <form className="idForm" action="#">
+            <label className="idLabel" htmlFor="id">
+              ID Único
+            </label>
+            <input
+              className="idInput"
+              type="text"
+              pattern="[0-9]*"
+              inputMode="numeric"
+              min="0"
+              max="9999"
+              autoFocus
+              required
+              ref={idInputRef}
+            />
+            <button onClick={handleClick} className="idSubmit" type="submit">
+              Verificar ID
+            </button>
+          </form>
+        </div>
+
+        {user == "" ? (
+          <></>
+        ) : (
+          <form
+            onSubmit={sendGoogleSheetData}
+            method="POST"
+            action="https://script.google.com/macros/s/AKfycbwo5DREVbk_S-YQrregCSnCeFsJr4mYVKOce0d9nq9obXFg-5hQv-iqx-5j7-3Mm7r5Tw/exec"
+            id="valeGelatoForm"
+            className="valeGelatoForm"
+          >
+            <label class="label" htmlFor="">
+              Nome
+            </label>
+            <input name="Nome" className="userInput" type="text" value={user} />
+
+            <input
+              name="Unidade"
+              className="userInput"
+              type="text"
+              value={unidadeText}
+            />
+            <div className="selectContainer">
+              <span className="selectTitle">Selecione o Produto</span>
+              <select className="userSelect" name="Item">
+                <option value="Gelato Pequeno">Gelato Pequeno</option>
+                <option value="Gelato Medio">Gelato Médio</option>
+                <option value="Gelato Grande">Gelato Grande</option>
+              </select>
+            </div>
+
+            {isFormSending ? (
+              <h3>Enviando...</h3>
+            ) : (
+              <button className="sendForm" type="submit">
+                Enviar
+              </button>
+            )}
+          </form>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Vales;
