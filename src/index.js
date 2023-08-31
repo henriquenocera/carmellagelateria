fd27999f278af2709b2bd82dd7e43708aa10601b
Henrique Nocera <henrique@axioscorp.com.br>
2023-08-31 17:30:53 -0300

    index

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ import ChecklistFechamento from "./routes/ChecklistFechamento.tsx";
 import NavBar from "./components/Navbar";
 import ValeGelato from "./routes/ValeGelato.tsx";
 import Vales from "./routes/Vales.tsx";
+import Perdas from "./routes/Perdas.tsx";
+
 
 const AppLayout = () => (
   <>
@@ -26,6 +28,7 @@ const router = createBrowserRouter(
       <Route path="/" element={<Home />} />
       <Route path="/vale-gelato" element={<ValeGelato />} />
       <Route path="/vales" element={<Vales />} />
+      <Route path="/perdas" element={<Perdas />} />
       <Route path="/checklist-abertura" element={<ChecklistAbertura />} />
       <Route path="/checklist-fechamento" element={<ChecklistFechamento />} />
     </Route>
