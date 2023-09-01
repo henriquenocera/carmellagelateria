e4b91aaa6b95277430f16725e63ed6d99edbb12f
Henrique Nocera <henrique@axioscorp.com.br>
2023-08-31 22:38:07 -0300

    index

---
+++ src/routes/Perdas.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useRef, useState } from "react";
+import "../css/Perdas.css";
+
+const telegramBotId = "5635956016:AAFzevSjVPEhTVsOEfLpbUsT0jni93pG6-c";
+const telegramChatId = "-1001602173856";
+
+function Perdas() {
+  return (
+    <>
+      <div className="container">
+        <h1>Perdas</h1>
+      </div>
+
+      <div className="container">
+        <h1>Em breve</h1>
+      </div>
+    </>
+  );
+}
+
+export default Perdas;
