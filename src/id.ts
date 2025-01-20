182203b2910d5ef02529ad8fcd9b261e077eab3a
Henrique Nocera <henocera@gmail.com>
2025-01-20 12:31:50 -0300

    i

--- src/id.ts
+++ src/id.ts
@@ -1,23 +1,22 @@
 export const ListId: readonly ListId[] = [
-    {
-      nome: "Henrique",
-      value: "2703",
-    },
-    {
-      nome: "Marina",
-      value: "1727",
-    },
-    {
-      nome: "Camila",
-      value: "7812",
-    },
-    {
-      nome: "Funcionário 2",
-      value: "888888",
-    },
-    {
-      nome: "Funcionário 3",
-      value: "999999",
-    },
-
+  {
+    nome: "Henrique",
+    value: "2703",
+  },
+  {
+    nome: "Marina",
+    value: "1727",
+  },
+  {
+    nome: "Camila",
+    value: "7812",
+  },
+  {
+    nome: "Geovana",
+    value: "2839",
+  },
+  {
+    nome: "Funcionário 3",
+    value: "999999",
+  },
 ];
