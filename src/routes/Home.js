60934fb7ce1b83aaea1e8142ea24bcf526c6d6ee
Henrique Nocera <henocera@gmail.com>
2025-02-22 13:56:58 -0300

    i

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -15,7 +15,7 @@ function Home() {
     const { data, error } = await supabase
       .from("Checklist")
       .select("*")
-      .eq("store", "ahu")
+      .eq("store", "batel")
       .limit(6)
       .order("id", { ascending: false });
 
