b007bbfb8359625970cb9b9b629ff1e5722a1e8b
Henrique Nocera <henocera@gmail.com>
2025-12-11 11:14:46 -0300

    i

---
+++ src/routes/Manual.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useRef, useState } from "react";
+import Select from "react-select";
+import "../css/Manual.css";
+
+
+
+const unidadeText = "Alto da XV";
+const unidade = "altoxv";
+
+function Manual() {
+  
+
+  return (
+    <>
+    <div className="container">
+    <h1>Manual</h1>
+
+    </div>
+    </>
+  )
+}
+
+export default Manual;
