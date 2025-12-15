335465552b2d54c5d70b348aec2a052241ea2416
Henrique Nocera <henocera@gmail.com>
2025-12-15 09:45:23 -0300

    i

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/logo.svg';
+import produto from './assets/waffle-nutella-morango.jpg'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+      <div className='container'>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2 className='title-h2'>Faça seu pedido abaixo</h2>
+      <p className='text'>Horário de funcionamento: <br></br> Seg - Sáb | 12:00 às 19:00 | </p>
+      <h3 className='title-h3'>Cardápio de Delivery</h3>
+      <div className='container-produtos'>
+        <div className='produto'>
+          <img src={produto} className="produto-img" alt="produto" />
+          <h4 className='produto-title'>Waffle de Nutella com Morango</h4>
+          <p className='produto-description'>Massa de waffle de liège com Nutella e morangos</p>
+          <p className='produto-price'>R$ 20,00</p>
+        </div>
+      </div>
+      </div>
+    </>
   );
 }
 
