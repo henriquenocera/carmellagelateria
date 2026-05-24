d76b70971b448637ef18388123a08eae3dc2c17e
Henrique Nocera <henocera@gmail.com>
2026-05-24 15:40:24 -0300

    teste

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,10 +1,10 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = env.REACT_APP_SUPABASE_URL
-const supabaseKey = env.REACT_APP_SUPABASE_KEY
+const supabaseUrl = import.meta.env.REACT_APP_SUPABASE_URL
+const supabaseKey = import.meta.env.REACT_APP_SUPABASE_KEY
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error("Variáveis de ambiente REACT_APP_SUPABASE_URL e VITE_SUPABASE_KEY são obrigatórias")
+  throw new Error("Variáveis de ambiente REACT_APP_SUPABASE_URL e REACT_APP_SUPABASE_KEY são obrigatórias")
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
