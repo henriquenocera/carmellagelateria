8f5167972515d2b9a9b278ffdf6a28c0b6f79e1c
Henrique Nocera <henocera@gmail.com>
2026-05-24 15:35:31 -0300

    change .env variables

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+const supabaseUrl = import.meta.env.REACT_APP_SUPABASE_URL
+const supabaseKey = import.meta.env.REACT_APP_SUPABASE_KEY
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Variáveis de ambiente VITE_SUPABASE_URL e VITE_SUPABASE_KEY são obrigatórias")
