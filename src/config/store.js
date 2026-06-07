c211b3c30b0451dc53a0386a9deb924810612f7b
Henrique Nocera <henocera@gmail.com>
2026-06-07 19:37:05 -0300

    new checklist itens

--- src/config/store.js
+++ src/config/store.js
@@ -1,17 +1,33 @@
 export const STORE_CONFIG = {
+
   // Chave usada no banco de dados (ex: no config_estoque do cadastro de insumos)
+
   // Homepage - Checklist table
+
   // Relatório Salgados
-  key: "ahu",
+
+  key: "altoxv",
+
+
 
   // Nome amigável usado para exibição e gravação em tabelas mais recentes (ex: "Loja Ahú")
-  name: "Loja Ahú",
+
+  name: "Loja Alto XV",
+
+
 
   // Variação usada nas queries do Supabase para relatórios antigos (ex: "Ahu")
+
   // Homepage - Vales table
+
   // Vales
-  textName: "Ahu",
+
+  textName: "Alto da XV",
+
+
 
   // Endereço da loja
-  address: "Rua Colombo, 183",
+
+  address: "Rua Sete de Abril, 934",
+
 };
